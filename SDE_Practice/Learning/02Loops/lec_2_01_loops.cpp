ed15c67f6173accb8854bc2220e1c345754efe6b
Ankit kumar <ankitkumar9864@gmail.com>
2022-11-11 12:53:05 +0530

    11-11-2022

---
+++ SDE_Practice/Learning/02Loops/lec_2_01_loops.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std ;
+
+int main(){
+    // int a;
+    // cin>>a;
+    // cout<<"Value of a is : " << n <<endl;
+
+
+    //int a,b;
+    //cin>>a>>b;
+    //cout<<"Value of a and b is :"<<a<<" "<<b <<endl;
+    
+    // if(a>0){
+    //     cout<<"A is Positive"<<endl;   
+    // }
+    // else{
+    //     cout<<"A is negative";
+    // }
+
+
+    // a = cin.get();
+    // cout<<"Value of a is :"<<a<<endl;
+    
+    int a ;
+    cout<<"Enter the value of a "<<endl ;
+    cin >> a;
+    
+    if(a>0){
+        cout<<"A is positive";
+    }
+    else{
+        if(a<0){
+            cout<<"A is negative";
+        }
+        
+    }
+}
---
+++ SDE_Practice/Learning/03if_else/lec_3_01_if_else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 9;
+    if(a == 9) {
+        cout<<"NINE ";
+    }
+    if(a>0){
+        cout<<"POSITIVE";
+    }
+    else{
+        cout<<"NEGATIVE";
+    }
+}
