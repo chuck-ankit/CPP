dcd5ea4ccb026b9bebc7e80ab281114a18bb97e7
Ankit kumar <66569159+chuck-ankit@users.noreply.github.com>
2023-01-20 22:28:41 +0530

    20_01_23 log

--- CodeChef/65_array_swaping_alternate_elements.cpp
+++ CodeChef/65_array_swaping_alternate_elements.cpp
@@ -5,8 +5,11 @@
 using namespace std;
 
 void swap_alternate(int arr[],int n){
-    for(int i =0;i<(n-1); i++){
-        swap(arr[i], arr[i+1]);
+    for(int i =0;i<(n-1); i+=2){
+        if(i+1 <n )
+        {
+            swap(arr[i], arr[i+1]);
+        }
     }
 }
 
