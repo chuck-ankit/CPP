23584a09b4e26ef717fb4d7db1fdc16b57588ae0
Ankit kumar <66569159+chuck-ankit@users.noreply.github.com>
2023-01-20 22:52:35 +0530

    Update 68_array_unique_finding.cpp

---
+++ CodeChef/68_array_unique_finding.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int findUnique(int *arr, int size)
+{
+    //Write your code here
+}
+
+int main(){
+    int t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[1000];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+    }
+}
