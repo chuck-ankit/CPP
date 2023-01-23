c87b8ff7db7af8e3789ce4766cabe571a1a88dde
Ankit kumar <66569159+chuck-ankit@users.noreply.github.com>
2023-01-23 18:54:31 +0530

    23_01_23 log

---
+++ CodeChef/74_basic_hoop_jump.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int ans = (n/2)+1;
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
