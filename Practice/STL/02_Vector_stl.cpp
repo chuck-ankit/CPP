9d023bad4f4371c2a98c3f75df38890c2b1207eb
Ankit kumar <66569159+chuck-ankit@users.noreply.github.com>
2023-01-24 17:05:07 +0530

    24_01_23 log

---
+++ Practice/STL/02_Vector_stl.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int> v;
+    cout<< "Capicity ->"<<v.capacity()<<endl;
+
+    v.push_back(10);
+    cout<< "Capicity ->"<<v.capacity()<<endl;
+
+    v.push_back(11);
+    cout<< "Capicity ->"<<v.capacity()<<endl;
+
+    v.push_back(12);
+    cout<< "Capicity ->"<<v.capacity()<<endl;
+
+    cout<<" Size ->" << v.size()<<endl;
+
+    cout<<" Element at Second Element ->" <<v.at(2)<<endl;
+}
