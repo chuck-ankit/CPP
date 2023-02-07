419d78c5c56def2b9a80b36c0cd4e29e5a1f1a55
Ankit kumar <66569159+chuck-ankit@users.noreply.github.com>
2023-02-08 02:10:16 +0530

    08_02_2023 Log

--- Practice/String/10_string-comparison.cpp
+++ Practice/String/10_string-comparison.cpp
@@ -25,3 +25,18 @@ Rules:
 5. !=: If any of the points from 1 to 3 follow up then, strings are said to be unequal.
 
 */
+
+#include <iostream>
+using namespace std;
+int main() {
+    string str1 = "Coding";
+    string str2 = " Ninjas!";
+    string str3 = "Coding";
+    bool res1 = str1 == str2;
+    bool res2 = str2 < str1;
+    bool res3 = str3 != str1;
+    cout << res1 << endl;
+    cout << res2 << endl;
+    cout << res3 << endl;
+    return 0;
+}
---
+++ Practice/rough.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=0;
+    int b=10;
+    a=2;
+    b=7;
+    if(a&&b){
+        cout<<"True: "<<endl;
+    }
+    else{
+        cout<<"False: "<<endl;
+    }
+
+    return 0;
+
+} // namespace std;
