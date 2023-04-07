34e2f14c6a9692145d1514ef1e9c603f7e2df7b4
Ankit kumar <ankitkumar9864@gmail.com>
2023-04-07 12:56:13 +0530

    07

---
+++ Practice/String/11_string_remainder_of_11.cpp
@@ -0,0 +1,53 @@
+///If the input is short
+
+
+
+
+
+
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+
+public:
+    int xmod11(string x)
+    {
+       // code here
+    long long  int a;
+    a = stoi(x);
+    long  long int ans = a % 11 ;
+    return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string x;
+        cin>>x;
+        Solution ob;
+        int ans = ob.xmod11(x);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
