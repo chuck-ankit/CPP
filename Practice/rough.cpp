9161a4a7391027bfcf153cc696f10b78cf5328cb
Ankit kumar <ankitkumar9864@gmail.com>
2024-04-25 10:14:43 +0530

    25_04_2024 Log

--- Practice/rough.cpp
+++ Practice/rough.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
 int main(){
-    int k=8;
-    [{
-        
-    }]
+    int c =0;
+    int a = (~c==1);
+    
+    cout<< (a) <<" ";
     return 0;
 
 } // namespace std;
